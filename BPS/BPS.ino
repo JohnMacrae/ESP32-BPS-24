d00866526a94f6d686894e97b77cf5852cc1e761
John <jramacrae@gmail.com>
2021-04-10 06:54:59 +0100

    Add ADC

---
+++ BPS/BPS.ino
@@ -0,0 +1,406 @@
+/*
+ * BPS - 24
+ */
+
+#define DEBUGLEVEL 3
+#define LED_BUILTIN 33
+
+#include <DebugUtils.h>
+#include "credentials.h"
+const long vers = 24000;
+
+#include <WiFi.h>
+#include <Wire.h>
+//#include <Adafruit_ADS1015.h>
+#include <ESPmDNS.h>
+#include <ArduinoOTA.h>
+#include <FS.h>
+#include <PubSubClient.h>
+#include "SPIFFS.h"
+#include <ArduinoJson.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <rom/rtc.h>
+#include <MCP342x.h>
+
+#define ESP32_CAN_TX_PIN GPIO_NUM_22 //for BMS was 16
+#define ESP32_CAN_RX_PIN GPIO_NUM_23 // was 4
+#define BatUpdatePeriod 2000
+
+//#define N2k_CAN_INT_PIN 21
+#include "NMEA2000_CAN.h"       // This will automatically choose right CAN library and create suitable NMEA2000 object
+#include "N2kMessages.h"
+#include "NMEA2K.h"
+
+#define CELL1       "bps/cell1"
+#define CELL2       "bps/cell2"
+#define CELL3       "bps/cell3"
+#define CELL4       "bps/cell4"
+
+#define CELL5       "bps/cell1"
+#define CELL6       "bps/cell2"
+#define CELL7       "bps/cell3"
+#define CELL8       "bps/cell4"
+
+#define BANK        "bps/bank"
+#define DELTA       "bps/delta"
+#define HICELL      "bps/hicell"
+#define LOCELL      "bps/locell"
+#define CELLSUM     "bps/cellsum"
+#define STATUS      "bps/status"
+#define LVCLED      "bps/LVCled"
+#define HVCLED      "bps/HVCled"
+#define CUTOFF      "bps/cutoff"
+#define UPTIME      "bps/uptime"
+#define TEMP1       "bps/temp1"
+#define TEMP2       "bps/temp2"
+#define ALARM       "bps/alarm"
+#define DELTASUM    "bps/deltaSum"
+
+#define FORMAT_SPIFFS_IF_FAILED true
+
+//#define  TESTING 1
+
+int inc = 1;
+
+long runcounter = 0;
+bool defaultsettings = true;
+bool outputTest = false;
+unsigned long uptime;
+unsigned long reportTimer = 0;
+
+int pulseLength = 1000;
+
+/*
+   Alarmcodes
+*/
+int normal = 0;
+int lowchirp = 1;
+int chirp = 2;
+int highchirp = 3;
+
+const int lowcut = 20;
+const int lowon = 21;
+const int highcut = 30;
+const int highon = 31;
+//Temperature warnings
+int tempWarn = 40; //High temperature warning if T>40
+int tooHot = 50;
+
+/*
+   Pins
+*/
+
+const int LVCONPIN = 17;
+const int LVCOFFPIN = 5;
+
+const int LEDPINLVC = 26;
+const int LEDPINHVC = 32;
+
+const int HVCOFFPIN = 4;
+const int HVCONPIN = 16;
+
+const int mySCL = 25;
+const int mySDA = 21;
+
+const int GPIO2 = 18;
+const int GPIO1 = 19;
+
+const int TEMPSIG1 = 33;
+const int TEMPSIG2 = 14;
+
+const int BUZZERPIN = 13;
+
+const int TOUCH1 = 31;
+const int TOUCH2 = 30;
+const int TOUCH3 = 29;
+
+/*
+  const int JTAG1 = 13;
+  const int JTAG2 = 12;
+  const int JTAG3 = 14;
+  const int JTAG4 = 15;
+*/
+const int CANCELALARM = 13;
+
+const bool  wifi = true;
+
+char          WifiSSID[32];
+char          WifiKey[32];
+char          WifiAPKey[32];
+char          ControllerUser[32];
+char          ControllerPassword[32];
+char          MQTT_client[32];
+char          Broker[32];
+char          BrokerIP[32];
+long          Version;
+
+bool boot = true;
+/*
+   Values controlling behaviour of GPIO - Drivers
+   0 == do nothing
+   20 == operate with LVC in low - high - low
+   21 == operate with LVC stay high
+   22 == operate with LVC stay low
+
+   30 == operate with HVC in low - high - low
+   31 == operate with HVC stay high
+   32 == operate with HVC stay low
+
+*/
+int gpio_1 = 0;
+int gpio_2 = 0;
+
+/*
+   Defaults
+*/
+int oldbank = 26000;
+int olddelta = 0;
+int change = 0;
+int bank = 26000;
+int highcell = 0;
+int lowcell = 0;
+int16_t cellsum = 0;
+int highcellv = 0;
+int lowcellv = 0;
+int delta = 0;
+int Cell[] = {3350, 3350, 3350, 3350,3350, 3350, 3350, 3350};
+int cellAve[] = {3350, 3350, 3350, 3350,3350, 3350, 3350, 3350};
+int deltaSum = 0;
+
+#ifdef TESTING
+int testCell[] = {3300, 3300, 3340, 3300,3350, 3350, 3350, 3350};
+#endif
+
+int ChirpSilencePeriod = 30; //seconds
+unsigned long timervar = 0;
+
+long          high_cellwarn = 3500;
+long          high_cellcutoff = 3550;
+long          low_cellwarn = 2600;
+long          low_cellcutoff = 2500;
+long          high_bankwarn = 13850;
+long          high_bankcutoff = 14200;
+long          low_bankwarn = 12000;
+long          low_bankcutoff = 11500;
+long          deltawarn = 100;
+long          deltacutoff = 1250;
+
+int alarmLimit = 5;
+int alarmCount = 0;
+
+int  reportrate = 1;
+char msg[75];
+
+struct MQMessage {
+  char topic[32];
+  char message[48];
+};
+
+struct Button {
+  const uint8_t PIN;
+  unsigned long cancelTimer;
+  bool pressed;
+};
+
+Button cancel_alarm = {CANCELALARM, 0, false};
+
+//Adafruit_ADS1115 ads;
+// 0x68 is the default address for all MCP342x devices
+uint8_t address = 0x68;
+uint8_t address2 = 0x6A;
+
+MCP342x adc = MCP342x(address);
+MCP342x adc2 = MCP342x(address2);
+
+QueueHandle_t ChirpQueue, PrintQueue, MQ_Queue, ChirpSilenceQ;
+TaskHandle_t TaskA, TaskB, TaskC, TaskD , TaskE;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(TEMPSIG1);
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+int deviceCount = 0;
+float tempC;
+int temperatures[2];
+
+/*
+   RTC Data Partition
+*/
+RTC_DATA_ATTR char timeOutCounter = 0;
+
+void runEachTimer(int oldAlarmStatus)
+{
+  timervar = millis() + (reportrate * 1000);
+
+  DEBUGPRINTLN3("Read Voltages");
+
+  readVoltage();
+
+  int alarmstatus = 0;
+  alarmstatus = (int) calculate_alarms();
+  oldAlarmStatus = alarmstatus;
+
+  runalarms(alarmstatus, oldAlarmStatus);
+
+  int answer = 100;
+  answer = (readled(LEDPINLVC) == 0) ? 1 : 0;
+  snprintf (msg, 75, "%d", answer);
+  MQ_Publish(LVCLED, msg);
+
+  answer = (readled(LEDPINHVC) == 0) ? 1 : 0;
+  snprintf (msg, 75, "%d", answer);
+  MQ_Publish(HVCLED, msg);
+
+  getTemperatures();
+  runTemperatureAlarm();
+
+  snprintf (msg, 75, "%f", temperatures[0]);
+  MQ_Publish(TEMP1, msg);
+  snprintf (msg, 75, "%f", temperatures[1]);
+  MQ_Publish(TEMP2, msg);
+
+  updateLed();
+  DEBUGPRINTLN3("Update Led Line");
+
+  ArduinoOTA.handle();
+  client.loop();
+}
+
+void setup()
+{
+  setup_interrupts();
+  PinSetup();
+  OneWireSetup();
+
+  ChirpQueue = xQueueCreate( 10, sizeof( int ));
+  PrintQueue = xQueueCreate( 10, 32);
+  MQ_Queue = xQueueCreate(750, sizeof(struct MQMessage));
+  // ChirpSilenceQ = xQueueCreate(5, sizeof( bool ));
+
+  Serial.begin(115200);
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+    DEBUGPRINTLN3("Failed to mount file system");
+    return;
+  }
+
+  loadConfig();
+
+  rebootBuzz();
+
+  //WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.setHostname(Host);
+  WiFi.begin(ssid, password);
+  vTaskDelay(500);
+
+  /* configure the MQTT server with IPaddress and port */
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(receivedCallback);
+  wifiReconnect();
+
+  OTA_Setup();
+  Wire.begin( mySDA, mySCL);
+  ADC_Setup();
+
+  xTaskCreatePinnedToCore(
+    MQTT_Handle,
+    "MQTT_Task",
+    8192,
+    NULL,
+    5,
+    &TaskD,
+    tskNO_AFFINITY);//0
+
+  vTaskDelay(500);
+
+  xTaskCreatePinnedToCore(
+    ServiceAlarmQueue,
+    "ChirpTask",
+    8192,
+    NULL,
+    6,
+    &TaskB,
+    tskNO_AFFINITY);//1
+
+  vTaskDelay(500);
+  /*
+    xTaskCreatePinnedToCore(
+    SerialPrintTask,
+    "PrintTask",
+    8192,
+    NULL,
+    6,
+    &TaskC,
+    tskNO_AFFINITY);//1
+
+    vTaskDelay(500);
+  */
+  /*
+    xTaskCreatePinnedToCore(
+     ChirpSilence,
+     "ChirpSilence",
+     8192,
+     NULL,
+     6,
+     &TaskE,
+     1);//1
+    vTaskDelay(500);
+
+    DEBUGPRINTLN3(uxTaskGetNumberOfTasks());
+    DEBUGPRINTLN3(eTaskGetState(TaskA));
+    DEBUGPRINTLN3(eTaskGetState(TaskB));
+    //DEBUGPRINTLN3(eTaskGetState(TaskC));
+    DEBUGPRINTLN3(eTaskGetState(TaskD));
+    //DEBUGPRINTLN3(eTaskGetState(TaskE));
+    DEBUGPRINTLN3("SetupCompleted");
+
+    updateLed();
+  */
+
+  //vTaskDelay(1000);
+
+  xTaskCreatePinnedToCore(
+    Task1,
+    "RunTimer_Task",
+    8192,
+    NULL,
+    6,
+    &TaskA,
+    1);//1
+
+  vTaskDelay(1000);
+
+  //Serial.println("CPU0 reset reason:");
+  //print_reset_reason(rtc_get_reset_reason(0));
+  // verbose_print_reset_reason(rtc_get_reset_reason(0));
+
+  //Serial.println("CPU1 reset reason:");
+  //print_reset_reason(rtc_get_reset_reason(1));
+  // verbose_print_reset_reason(rtc_get_reset_reason(1));
+
+  print_reset_reason(rtc_get_reset_reason(0));
+  print_reset_reason(rtc_get_reset_reason(1));
+  DEBUGPRINTLN3(timeOutCounter);
+
+  N2Ksetup();// Initialise N2K
+
+  snprintf (msg, 75, "Reset: %u", timeOutCounter );
+  MQ_Publish("bps/timeOutCounter", msg);
+}
+
+void loop(void)
+{
+
+  client.loop();
+  ArduinoOTA.handle();
+  SendN2kBattery();
+  NMEA2000.ParseMessages();
+
+}
---
+++ BPS/credentials.h
@@ -0,0 +1,12 @@
+/*
+ * Credentials here
+ * 
+ */
+ 
+const char* ssid = "agboat_s";
+const char* password = "jram7757";
+const char* Host = "BPS32";
+const char* MQ_client = "BPS";           // your MQTT Client ID
+const char* MQ_user = "jramacrae";       // your MQTT password
+const char* MQ_pass = "jram7757";        // your network password
+const char* mqtt_server = "192.168.199.8";
